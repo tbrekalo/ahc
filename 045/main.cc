4d026b025ac16c91998934a5a4482624cceae5dc
tbrekalo <t@tbrekalo.xyz>
2025-03-30 20:34:37 +0200

    045: coord setup

--- 045/main.cc
+++ 045/main.cc
@@ -11,6 +11,92 @@
 
 namespace tbrekalo {
 
+// https://prng.di.unimi.it/xoshiro256plusplus.c
+class Xoshiro256pp {
+  static constexpr std::array<std::uint64_t, 4> kJump128{
+      0x180ec6d33cfd0aba, 0xd5a61266f0c9392c, 0xa9582618e03fc9aa,
+      0x39abdc4529b1661c};
+
+  static constexpr std::array<std::uint64_t, 4> kJump192{
+      0x76e15d3efefdcbbf, 0xc5004e441c522fb3, 0x77710069854ee241,
+      0x39109bb02acbe635};
+
+  [[gnu::always_inline]] static constexpr std::uint64_t rotl(
+      const std::uint64_t x, int k) {
+    return (x << k) | (x >> (64 - k));
+  }
+
+  constexpr auto jumpImpl(std::array<std::uint64_t, 4> const& jump)
+      -> Xoshiro256pp {
+    auto result = *this;
+    std::uint64_t s0 = 0;
+    std::uint64_t s1 = 0;
+    std::uint64_t s2 = 0;
+    std::uint64_t s3 = 0;
+    for (std::uint64_t j : jump) {
+      for (int b = 0; b < 64; ++b) {
+        if (j & (static_cast<std::uint64_t>(1) << b)) {
+          s0 ^= s_[0];
+          s1 ^= s_[1];
+          s2 ^= s_[2];
+          s3 ^= s_[3];
+        }
+        next();
+      }
+    }
+    s_[0] = s0;
+    s_[1] = s1;
+    s_[2] = s2;
+    s_[3] = s3;
+
+    return result;
+  }
+
+  std::array<std::uint64_t, 4> s_;
+
+ public:
+  using result_type = std::uint64_t;
+  using value_type = std::array<std::uint64_t, 4>;
+
+  constexpr Xoshiro256pp() = delete;
+  constexpr explicit Xoshiro256pp(value_type const& state) : s_(state) {}
+
+  constexpr auto next() noexcept -> result_type {
+    const std::uint64_t result = rotl(s_[0] + s_[3], 23) + s_[0];
+    const std::uint64_t t = s_[1] << 17;
+
+    s_[2] ^= s_[0];
+    s_[3] ^= s_[1];
+    s_[1] ^= s_[2];
+    s_[0] ^= s_[3];
+
+    s_[2] ^= t;
+
+    s_[3] = rotl(s_[3], 45);
+
+    return result;
+  }
+
+  constexpr auto operator()() noexcept -> result_type { return next(); }
+
+  /* This is the jump function for the generator. It is equivalent
+     to 2^128 calls to next(); it can be used to generate 2^128
+     non-overlapping subsequences for parallel computations. */
+  constexpr auto jump() noexcept -> Xoshiro256pp { return jumpImpl(kJump128); }
+
+  /* This is the long-jump function for the generator. It is equivalent to
+     2^192 calls to next(); it can be used to generate 2^64 starting points,
+     from each of which jump() will generate 2^64 non-overlapping
+     subsequences for parallel distributed computations. */
+  constexpr auto longJump(void) noexcept -> Xoshiro256pp {
+    return jumpImpl(kJump192);
+  }
+};
+
+}  // namespace tbrekalo
+
+namespace tbrekalo {
+
 static constexpr int kMaxXY = 10'000;
 static constexpr int kBlockLength = 1'000;
 
@@ -42,12 +128,25 @@ struct Group {
   std::vector<Road> roads;
 };
 
-struct SpatialId {
+struct ClusterId {
   int x;
   int y;
 
-  friend constexpr auto operator<=>(SpatialId lhs, SpatialId rhs) noexcept
+  friend constexpr auto operator<=>(ClusterId lhs, ClusterId rhs) noexcept
       -> std::strong_ordering = default;
+};
+
+struct Coord {
+  int x;
+  int y;
+
+  friend constexpr auto operator<=>(Coord, Coord) noexcept -> Coord = default;
+  friend constexpr auto operator+(Coord lhs, Coord rhs) noexcept -> Coord {
+    return {
+        .x = lhs.x + rhs.x,
+        .y = lhs.y + rhs.y,
+    };
+  };
 };
 
 static auto LoadProblem(std::istream& istrm) -> Problem {
@@ -128,7 +227,14 @@ static auto NaiveGroupRoads(std::span<int const> members) -> std::vector<Road> {
   return dst;
 }
 
-static auto BoxSpatialId(int block_length, Box box) -> SpatialId {
+;
+
+static auto BoxAvgCoord(Box box) -> Coord {
+  return Coord{.x = box.lx + (box.rx - box.lx) / 2,
+               .y = box.ly + (box.ry - box.ly) / 2};
+}
+
+static auto BoxClusterId(int block_length, Box box) -> ClusterId {
   assert(block_length != 0);
 
   return {
@@ -137,11 +243,11 @@ static auto BoxSpatialId(int block_length, Box box) -> SpatialId {
   };
 }
 
-static auto SpatialIdDist(SpatialId lhs, SpatialId rhs) -> int {
+static auto ClusterlIdDist(ClusterId lhs, ClusterId rhs) -> int {
   return (lhs.x - rhs.x) * (lhs.x - rhs.x) + (lhs.y - rhs.y) * (lhs.y - rhs.y);
 }
 
-static auto SpatialGrouping(std::istream&, std::ostream&,
+static auto NaiveClustering(std::istream&, std::ostream&,
                             Problem const& problem) -> std::vector<Group> {
   struct GroupSpec {
     int id;
@@ -167,14 +273,26 @@ static auto SpatialGrouping(std::istream&, std::ostream&,
     std::sort(buffer.begin() + 1, buffer.end(),
               [block_length, &boxes = problem.boxes, ref = *buffer.begin()](
                   int a, int b) {
-                return SpatialIdDist(BoxSpatialId(block_length, boxes[a]),
-                                     BoxSpatialId(block_length, boxes[ref])) <
-                       SpatialIdDist(BoxSpatialId(block_length, boxes[b]),
-                                     BoxSpatialId(block_length, boxes[ref]));
+                return ClusterlIdDist(BoxClusterId(block_length, boxes[a]),
+                                      BoxClusterId(block_length, boxes[ref])) <
+                       ClusterlIdDist(BoxClusterId(block_length, boxes[b]),
+                                      BoxClusterId(block_length, boxes[ref]));
               });
     std::copy(buffer.begin(), buffer.begin() + size,
               std::back_inserter(groups[group_id].members));
     buffer.erase(buffer.begin(), buffer.begin() + size);
+  }
+
+  return groups;
+}
+
+static auto OptimizeGroups(std::istream& istrm, std::ostream& ostrm,
+                           Problem const& problem, std::vector<Group> groups)
+    -> std::vector<Group> {
+  std::vector<Coord> centers(groups.size(), {0, 0});
+  for (int i = 0; i < groups.size(); ++i) {
+    for (int j = 0; j < groups[i].members.size(); ++j) {
+    }
   }
 
   return groups;
@@ -217,7 +335,7 @@ namespace tb = tbrekalo;
 auto main(void) -> int {
   auto problem = tb::LoadProblem(std::cin);
   auto groups = tb::OptimizeRoads(
-      std::cin, std::cout, tb::SpatialGrouping(std::cin, std::cout, problem),
+      std::cin, std::cout, tb::NaiveClustering(std::cin, std::cout, problem),
       problem.q, problem.l);
 
   tb::PrintSolution(std::cout, groups);
